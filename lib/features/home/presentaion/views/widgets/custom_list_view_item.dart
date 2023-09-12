6c8fc9adfc2f2c1ac8eabee2e452401dd478b522
eiad <eiadhany04@gmail.com>
2023-09-12 21:11:38 +0300

    add Featured BooksList View to home

--- lib/features/home/presentaion/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentaion/views/widgets/custom_list_view_item.dart
@@ -2,21 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/uitls/assets.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: const DecorationImage(
-                  image: AssetImage(AssetsData.book), fit: BoxFit.fill)),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: const DecorationImage(
+                image: AssetImage(AssetsData.book), fit: BoxFit.fill)),
       ),
     );
   }
