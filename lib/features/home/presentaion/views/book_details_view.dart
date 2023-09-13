9f8e6994ae362ca4586dcb26e1cb90a16bbe0020
eiad <eiadhany04@gmail.com>
2023-09-13 21:38:04 +0300

    create  book details view

---
+++ lib/features/home/presentaion/views/book_details_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
--- lib/features/home/presentaion/views/home_view.dart
+++ lib/features/home/presentaion/views/home_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/home/presentaion/views/home_view_body.dart';
+import 'package:bookly_app/features/home/presentaion/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
