9786ebe36c543f33948ec163ed802742f6e675e7
eiad <eiadhany04@gmail.com>
2023-09-19 00:47:15 +0300

    create server failure from dio

--- lib/core/uitls/errors.dart
+++ lib/core/uitls/errors.dart
@@ -1,3 +1,32 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout with api server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('send Timeout with api server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('recevie Timeout with api server');
+      case DioExceptionType.badResponse:
+        return ServerFailure('oops there is an error with api server');
+      case DioExceptionType.cancel:
+        return ServerFailure('request with api server is cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('No internet Connection');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate with api server');
+      case DioExceptionType.unknown:
+        return ServerFailure('Unknown with api server');
+    }
+  }
+}
--- lib/features/home/date/repos/home_repo_impl.dart
+++ lib/features/home/date/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/uitls/errors.dart';
 import 'package:bookly_app/features/home/date/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/date/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -19,7 +20,10 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
